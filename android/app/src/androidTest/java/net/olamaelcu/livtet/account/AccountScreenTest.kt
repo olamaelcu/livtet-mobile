55a237b5b6d35b1d37e730e0d2e018708fd2ce4f
Jacky Alcine <yo@jacky.wtf>
2026-07-31 22:03:27 -0400

    test(auth): add AccountScreen instrumentation test, verify all flavors compile

---
+++ android/app/src/androidTest/java/net/olamaelcu/livtet/account/AccountScreenTest.kt
@@ -0,0 +1,24 @@
+package net.olamaelcu.livtet.account
+
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import net.olamaelcu.livtet.DashboardActivity
+import org.junit.Rule
+import org.junit.Test
+
+class AccountScreenTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<DashboardActivity>()
+
+    @Test
+    fun `account screen shows sign-in prompt when signed out`() {
+        composeTestRule.onNodeWithText("Account").assertExists()
+        composeTestRule.onNodeWithText("Sign in to unlock social features").assertExists()
+    }
+
+    @Test
+    fun `ATProto sign-in button is present`() {
+        composeTestRule.onNodeWithText("Sign in with AT Protocol").assertExists()
+    }
+}
